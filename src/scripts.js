4d9dd04975415c4a0fed08f3298e19e4afdf597a
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-02-11 15:41:19 -0600

    add recipes to ui

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,30 @@
-console.log('Hello world');
+let allRecipes = {}
 const nav = document.querySelector('#nav');
 const userKitchen = document.querySelector('#userDropdown');
+const recipeChart = document.querySelector('#recipes');
 
 nav.addEventListener('click', navPress)
+window.addEventListener('load', instantiate)
+
+function instantiate() {
+  instantiateRecipeRepository();
+  console.log(allRecipes)
+  showRecipeImages();
+}
+
+function instantiateRecipeRepository() {
+  const recipes = recipeData.map(recipe => {
+    var recipe = new Recipe(recipe)
+    return recipe;
+  })
+  allRecipes = new RecipeRepository(recipes)
+}
+
+function showRecipeImages() {
+  allRecipes.recipes.forEach(recipe => {
+    recipeChart.innerHTML += `<img src=${recipe.image} alt=${recipe.name}>`
+  })
+}
 
 function navPress() {
   if (event.target.id === 'whatsCookin') {
